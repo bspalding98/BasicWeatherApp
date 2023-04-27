74b7f1ceca3a0bfba7fa1bd9344fd7dcf01ff2f9
bspalding98 <boydiespalding98@gmail.com>
2023-04-27 14:31:01 +1000

    javascript backend

---
+++ BasicWeatherApp/index.js
@@ -0,0 +1,62 @@
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-box button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weatherDetails');
+const Error404 = document.querySelector('.not-found');
+
+search.addEventListener('click', () =>{
+
+    const APIKey = '';
+    const city = document.querySelector('.search-box input').value;
+
+    if (city === '')
+        return;
+
+    fetch('').then(response => response.json()).then(json => {
+
+        if (json.cod === '404') {
+            container.style.height = '400px';
+            weatherBox.style.display = 'none';
+            weatherDetails.style.display = 'none';
+            Error404.style.display = 'block';
+            Error404.classList.add('fadeIn');
+            return;
+        }
+
+        Error404.style.display = 'none';
+        Error404.classList.add('fadeIn');
+
+        const image = document.querySelector('.weather-box img');
+        const temperature = document.querySelector('.weather-box .temperature');
+        const description = document.querySelector('.weather-box .description');
+        const humidity = document.querySelector('.weather-box .humidity span');
+        const wind = document.querySelector('.weather-box .wind span');
+
+        swtich (json.weather[0].main) {
+            case 'Clear':
+                image.src = 'images/clear.png';
+                break;
+
+            case 'Rain':
+                image.src = 'images/rain.png';
+                break;
+
+            case 'Snow':
+                image.src = 'images/snow.png';
+                break;
+
+            case 'Clouds':
+                image.src = 'images/clouds.png';
+                break;
+
+            case 'Haze':
+                image.src = 'images/haze.png';
+                break;
+
+            default:
+                image.src = '';
+        }
+
+        temperature.innerHTML = '${parseInt(json.main.temp)}<span>℃</span>';
+    })
+})
