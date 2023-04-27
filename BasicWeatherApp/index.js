090de613b484deebf2ec4dca2c786fcefb02c322
Boyd-Spalding <55310578+Boyd-Spalding@users.noreply.github.com>
2023-04-27 19:56:25 +1000

    more refactoring

--- BasicWeatherApp/index.js
+++ BasicWeatherApp/index.js
@@ -2,31 +2,33 @@ const container = document.querySelector('.container');
 const search = document.querySelector('.search-box button');
 const weatherBox = document.querySelector('.weather-box');
 const weatherDetails = document.querySelector('.weatherDetails');
-const Error404 = document.querySelector('.not-found');
+const error404 = document.querySelector('.not-found');
 
 search.addEventListener('click', () =>{
 
-    const APIKey = '';
+    const APIKey = 'dabfdac2dadbe275a7c2cf8a1ef15369';
     const city = document.querySelector('.search-box input').value;
 
     if (city === '')
         return;
 
-    fetch('').then(response => response.json()).then(json => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
+        .then(response => response.json())
+        .then(json => {
 
         if (json.cod === '404') {
             container.style.height = '400px';
             weatherBox.style.display = 'none';
             weatherDetails.style.display = 'none';
-            Error404.style.display = 'block';
-            Error404.classList.add('fadeIn');
+            error404.style.display = 'block';
+            error404.classList.add('fadeIn');
             return;
         }
 
-        Error404.style.display = 'none';
-        Error404.classList.add('fadeIn');
+        error404.style.display = 'none';
+        error404.classList.add('fadeIn');
 
-        const image = document.querySelector('.weather-box img'); // Need to check this is the correct syntax
+        const image = document.querySelector('.weather-box img'); 
         const temperature = document.querySelector('.weather-box .temperature');
         const description = document.querySelector('.weather-box .description');
         const humidity = document.querySelector('.weather-details .humidity span');
