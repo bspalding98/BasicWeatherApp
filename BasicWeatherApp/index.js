6ddc41e26dfe4f406d160d1c7bf9764f64acf9a1
bspalding98 <boydiespalding98@gmail.com>
2023-04-27 15:01:24 +1000

    refining code

--- BasicWeatherApp/index.js
+++ BasicWeatherApp/index.js
@@ -26,7 +26,7 @@ search.addEventListener('click', () =>{
         Error404.style.display = 'none';
         Error404.classList.add('fadeIn');
 
-        const image = document.querySelector('.weather-box img');
+        const image = document.querySelector('.weather-box img'); // Need to check this is the correct syntax
         const temperature = document.querySelector('.weather-box .temperature');
         const description = document.querySelector('.weather-box .description');
         const humidity = document.querySelector('.weather-details .humidity span');
@@ -57,7 +57,7 @@ search.addEventListener('click', () =>{
                 image.src = '';
         }
 
-        temperature.innerHTML = `${parseInt(json.main.temp)}<span>℃</span>`;
+        temperature.innerHTML = `${parseInt(json.main.temp)}<span>℃</span>`;   // innerHTML - add into the html field of temperature
         description.innerHTML = `${json.weather[0].description}`;
         humidity.innerHTML = `${json.main.humidity}}%`;
         wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
